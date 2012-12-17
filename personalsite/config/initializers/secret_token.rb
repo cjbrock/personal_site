358e6d2983ce19a710ae721f870c1ccab5e13308
Corinna Brock <corinna.brock@gmail.com>
2012-12-17 10:10:36 -0500

    working on impress.js

---
+++ personalsite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalsite::Application.config.secret_token = 'df558074384ef20c08a32465f8cbae43b0bb6a6f9c867e751732f90ca6461728429008d2b836ea528518afef6798b9069fe07f528117ea3d254d30f9b931ce77'
